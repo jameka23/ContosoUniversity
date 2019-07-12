19f1cb26f31769b0a25a61a451d3ad6a65bad6a7
Jameka Echols <jameka.echols@gmail.com>
2019-07-12 11:30:56 -0500

    added the models

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        //attribute lets you enter the primary key for the course rather than having the database generate it.
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] 
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        // navigation properties
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; } // note that the EnrollmentID could also just be ID
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; } // grade is of enum type and nullable value
+        
+        // navigation properties
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
