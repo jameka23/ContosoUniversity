9929ebe352962f834ad57071697fdc484dcabf75
Jameka Echols <jameka.echols@gmail.com>
2019-07-15 12:00:14 -0500

    complex related data

--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -12,12 +12,12 @@ namespace ContosoUniversity.Models
 
     public class Enrollment
     {
-        public int EnrollmentID { get; set; } // note that the EnrollmentID could also just be ID
+        public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
-        public Grade? Grade { get; set; } // grade is of enum type and nullable value
-        
-        // navigation properties
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
         public Course Course { get; set; }
         public Student Student { get; set; }
     }
---
+++ ContosoUniversity/ContosoUniversity/Models/OfficeAssignment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        //There's a one-to-zero-or-one relationship 
+        //between the Instructor and the OfficeAssignment entities.
+
+
+        //You can also use the Key attribute if the entity does 
+        //have its own primary key but you want to name the property something
+        [Key]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public Instructor Instructor { get; set; }
+    }
+}
